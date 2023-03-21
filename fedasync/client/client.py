38b982cda3a0135d18a40f4e4ca3d3ddf993f77b
dungcao <ctdung258@gmail.com>
2023-03-21 22:03:42 +0700

    implementing client ....

--- fedasync/client/client.py
+++ fedasync/client/client.py
@@ -1,31 +1,13 @@
 from abc import ABC, abstractmethod
-from pika import BlockingConnection
-from .queue_connector import QueueConnector
+from queue_connector import AsynRabbitMQConsumer
 
-
-
-class Client(ABC):
+class ClientModel(ABC):
     """
     - This is the abstract Client class
     - Client can extend to use with Deep frameworks like tensorflow, pytorch by extending this abstract class and 
         implement it's abstract methods. 
     """
-    def __init__(self, queue_connection: BlockingConnection) -> None:
-        super().__init__()
-        
-                
-        # Dependencies
-        self.queue_connector : QueueConnector = QueueConnector(queue_connection)
-        
-    
-    def join_server(self) -> None:
-        """
-        - Implement the logic for client here.
-        """
-        pass
-    
-    
-    
+
     # Abstract methods     
     @abstractmethod
     def set_weights(self, weights):
@@ -36,19 +18,14 @@ class Client(ABC):
         pass
 
     @abstractmethod
-    def train(self):
+    def train_step(self, return_gradient=False):
         pass
 
     @abstractmethod
     def evaluate(self):
         pass
 
-    @abstractmethod
-    def data_preprocessing(self):
-        pass
 
-    @abstractmethod
-    def create_model(self):
-        pass
-
+def start_client(client:ClientModel, queue_config):
+    pass
 
