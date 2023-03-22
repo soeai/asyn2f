37e1b993e700bc35943566dacf80c56f83934958
daovanngochoang <hoangdao.dev@gmail.com>
2023-03-22 21:08:08 +0700

    Create Message serializer and deserializer

--- fedasync/commons/messages/client_notify_model_to_server.py
+++ fedasync/commons/messages/client_notify_model_to_server.py
@@ -4,14 +4,14 @@ from fedasync.commons.messages.message import Message
 class ClientNotifyModelToServer(Message):
 
     def __init__(self, message):
-        self.client_id: ""
-        self.model_id: ""
-        self.global_model_version_used: 1
-        self.timestamp: 1213214
-        self.loss_value: 1.42
-        self.link: "minio/model1"
-        self.performance: ""
-        self.batch_size: ""
+        self.client_id =  ""
+        self.model_id =  ""
+        self.global_model_version_used =  1
+        self.timestamp =  1213214
+        self.loss_value =  1.42
+        self.link =  "minio/model1"
+        self.performance =  ""
+        self.batch_size =  ""
 
         # Run deserialize message function.
         self.deserialize(message)
--- fedasync/commons/messages/server_notify_model_to_client.py
+++ fedasync/commons/messages/server_notify_model_to_client.py
@@ -3,12 +3,12 @@ from .message import Message
 
 class ServerNotifyModelToClient(Message):
     def __init__(self, message):
-        self.model_id: ""
-        self.global_model_version: ""
-        self.global_model_update_data_size: 3424
-        self.timestamp: 134141
-        self.avg_loss: 0.2
-        self.chosen_id: ""
-        self.global_model_link: ""
+        self.model_id = ""
+        self.global_model_version = ""
+        self.global_model_update_data_size = 3424
+        self.timestamp = 134141
+        self.avg_loss = 0.2
+        self.chosen_id = ""
+        self.global_model_link = ""
 
         self.deserialize(message)
