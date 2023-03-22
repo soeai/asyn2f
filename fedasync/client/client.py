93e0ec74d5aa160b687e2ce422702468c5993640
dungcao <ctdung258@gmail.com>
2023-03-22 11:22:10 +0700

    implementing client ....

--- fedasync/client/client.py
+++ fedasync/client/client.py
@@ -1,31 +1,25 @@
-from abc import ABC, abstractmethod
 from queue_connector import AsynRabbitMQConsumer
+from model import ClientModel
+import json
 
-class ClientModel(ABC):
-    """
-    - This is the abstract Client class
-    - Client can extend to use with Deep frameworks like tensorflow, pytorch by extending this abstract class and 
-        implement it's abstract methods. 
-    """
 
-    # Abstract methods     
-    @abstractmethod
-    def set_weights(self, weights):
-        pass
-
-    @abstractmethod
-    def get_weights(self):
-        pass
-
-    @abstractmethod
-    def train_step(self, return_gradient=False):
-        pass
-
-    @abstractmethod
-    def evaluate(self):
-        pass
+class AsynRabbitMQConsumerImpl(AsynRabbitMQConsumer):
+    def on_message(self, _unused_channel, basic_deliver, properties, body):
+        msg = json.loads(body)
+        print(msg)
+        self.acknowledge_message(basic_deliver.delivery_tag)
 
 
 def start_client(client:ClientModel, queue_config):
-    pass
+    queue = AsynRabbitMQConsumerImpl(queue_config['url'],
+                                     queue_config['queuename'],
+                                     queue_config['routingkey'],
+                                     queue_config['exchange'])
+
+    queue.run()
+
+    print('DO TRAINING HERE')
+    client.train()
+    # sent notify
+
 
