54c10f49ff8a09abd8f6f83f68a16da2de8de782
daovanngochoang <hoangdao.dev@gmail.com>
2023-03-31 23:45:45 +0700

    delete queue producer, change consumer to queue connector that can do 2 things as producer and consumer, fix client to the right order of inheritance

--- fedasync/client/client_model.py
+++ fedasync/client/client_model.py
@@ -1,28 +1,27 @@
+import threading
 from abc import ABC, abstractmethod
 
-from fedasync.client.client import ClientProducer
 from fedasync.client.client_storage_connector import ClientStorage
+from fedasync.client.queue_manager import ClientQueueConnector
 
 
 class ClientModel(ABC):
-    """
-    - This is the abstract Client class
-    - Client can extend to use with Deep frameworks like tensorflow, pytorch by extending this abstract class and
-        implement it's abstract methods.
-    """
 
     def __init__(self, model):
         # Dependencies
         self._storage_connector: ClientStorage = None
-        self._producer: ClientProducer = None
+        self.queue_connector = ClientQueueConnector(self)
 
         self.model = model
         self.global_model_update_data_size = 0
-        self.avg_loss = 0.0
+        self.global_avg_loss = 0.0
         self.global_model_name = None
         self.global_model_version = 0
         self.local_version = 0
         self.__new_model_flag = False
+
+    def run(self):
+        self.queue_connector.run()
 
     # Abstract methods
     @abstractmethod
