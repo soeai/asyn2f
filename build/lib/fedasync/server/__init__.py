b3d87db230eb10a127fd427a684732f43ed6aaa3
daovanngochoang <hoangdao.dev@gmail.com>
2023-05-15 17:09:23 +0700

    clean up file structure

--- build/lib/fedasync/server/__init__.py
+++
@@ -1,4 +0,0 @@
-from .server import Server
-from .server_storage_connector import ServerStorage
-from .worker_manager import WorkerManager
-
--- build/lib/test/partial_tests/client_consumer.py
+++
@@ -1,6 +0,0 @@
-from fedasync.client.client import Client
-from fedasync.commons.conf import Config
-
-Config.QUEUE_NAME = "client_queue"
-client_consumer = Client()
-client_consumer.run_queue()
