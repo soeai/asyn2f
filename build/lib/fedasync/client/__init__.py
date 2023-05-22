4b17876940fc4aa6ce62baf18fedf5db9a5a8f7b
vtn13042000 <79910737+vtn13042000@users.noreply.github.com>
2023-05-22 09:08:13 +0700

    local commit

--- build/lib/fedasync/client/__init__.py
+++
@@ -1,5 +0,0 @@
-from .client import Client
-from .algorithms.client_asyncfL import ClientAsyncFl
-from .frameworks.tensorflow import TensorflowModel
-from .client_storage_connector import ClientStorage
-from .ModelWrapper import ModelWrapper
--- build/lib/test/partial_tests/Server.py
+++
@@ -1,9 +0,0 @@
-from fedasync.commons.conf import Config
-from fedasync.server.server import Server
-from fedasync.server.server_queue_connector import ServerQueueConnector
-
-Config.QUEUE_NAME = "server_queue"
-
-test_Server = ServerQueueConnector()
-
-test_Server.run_queue()
