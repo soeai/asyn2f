89bc09792686e21ca6278c7b20a9890a5eab261f
vtn13042000 <79910737+vtn13042000@users.noreply.github.com>
2023-05-23 14:31:29 +0700

    deploy tensorflow  client on mnist dataset to run as a demo

--- training_process/client/run_client.py
+++ training_process/client/run_client.py
@@ -1,6 +1,6 @@
 import os
-# import sys
-# sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.getcwd())))
 
 # platform package
 from asynfed.client.algorithms.client_asyncfL import ClientAsyncFl
