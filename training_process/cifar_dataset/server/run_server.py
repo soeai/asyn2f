9e36e5bac5b4a94bc48a54068a5ced0a3b2cfadb
vtn13042000 <thaonguyen.vuong.16@gmail.com>
2023-06-25 09:29:06 +0000

    add args for running server on cifar

--- training_process/cifar_dataset/server/run_server.py
+++ training_process/cifar_dataset/server/run_server.py
@@ -10,10 +10,11 @@ from asynfed.server.strategies.AsynFL import AsynFL
 
 # env config
 from dotenv import load_dotenv
+import argparse
+
 
 load_dotenv()
 # queue
-Config.QUEUE_URL = os.getenv("queue_url")
 # storage
 Config.STORAGE_ACCESS_KEY = os.getenv("access_key")
 Config.STORAGE_SECRET_KEY = os.getenv("secret_key")
@@ -26,26 +27,48 @@ Config.TMP_LOCAL_MODEL_FOLDER = os.getenv("local_model_folder")
 Config.TMP_GLOBAL_MODEL_FOLDER = os.getenv("global_model_folder")
 # Config.LOG_PATH = os.getenv("log_path")
 
+parser = argparse.ArgumentParser(description='Example script with command-line arguments.')
 
-if os.getenv("bucket_name"):
-    bucket_name = os.getenv("bucket_name")
-    print(f"choose bucket name as : {bucket_name}")
-else:
-    # bucket_name= 'test-client-tensorflow-mnist'
-    bucket_name= 'test-client-tensorflow-cifar10'
 
-if os.getenv("training_exchange"):
-    training_exchange = os.getenv("training_exchange")
-    print(f"choose training exchange as : {training_exchange}")
+# connection config
+parser.add_argument('--queue_url', dest='queue_url', type=str, help='specify the url of RabbitMQ server')
+parser.add_argument('--training_exchange', dest='training_exchange', type=str, help='define training exchange to connect to rabbitMQ server')
+parser.add_argument('--bucket_name', dest='bucket_name', type=str, help='define training exchange to connect to rabbitMQ server')
+
+args = parser.parse_args()
+
+if args.queue_url:
+    Config.QUEUE_URL = args.queue_url
 else:
-    # training_exchange= 'test-client-tensorflow-mnist'
-    training_exchange= 'test-client-tensorflow-cifar10'
+    Config.QUEUE_URL = os.getenv("queue_url")
+
+
+if args.bucket_name:
+    bucket_name = args.bucket_name
+else:
+    if os.getenv("bucket_name"):
+        bucket_name = os.getenv("bucket_name")
+        print(f"choose bucket name as : {bucket_name}")
+    else:
+        # bucket_name= 'test-client-tensorflow-mnist'
+        bucket_name= 'test-client-tensorflow-cifar10'
+
+
+if args.training_exchange:
+    Config.TRAINING_EXCHANGE = args.training_exchange
+else:
+    if os.getenv("training_exchange"):
+        training_exchange = os.getenv("training_exchange")
+        print(f"choose training exchange as : {training_exchange}")
+    else:
+        # training_exchange= 'test-client-tensorflow-mnist'
+        training_exchange= 'test-client-tensorflow-cifar10'
+
 
 # # load test dataset
 # from data_preprocessing import preprocess_dataset
 # default_testing_dataset_path = "../../data/cifar_data/test_set.pickle"
 # test_ds, _ = preprocess_dataset(default_testing_dataset_path, training = False)
-
 
 
 strategy = AsynFL()
