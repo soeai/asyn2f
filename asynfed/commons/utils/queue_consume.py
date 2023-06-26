205b5231a627d5850492f33c1f5f395aea9637ce
dungcao <ctdung258@gmail.com>
2023-06-26 08:40:40 +0700

    Separate consume and produce queue.

---
+++ asynfed/commons/utils/queue_consume.py
@@ -0,0 +1,49 @@
+import pika, json
+
+
+class Amqp_Consumer(object):
+    # Init an amqp client handling the connection to amqp servier
+    def __init__(self, configuration: dict, host_object: object = None):
+        self.host_object = host_object
+        self.exchange_name = configuration["exchange_name"]
+        self.exchange_type = configuration["exchange_type"]
+        self.routing_key = configuration["routing_key"]
+
+        # Connect to RabbitMQ host
+        if "amqps://" in configuration["end_point"]:
+            self.connection = pika.BlockingConnection(pika.URLParameters(configuration["end_point"]))
+        else:
+            self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=configuration["end_point"]))
+
+        # Create a channel
+        self.channel = self.connection.channel()
+
+        # Init an Exchange
+        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type=self.exchange_type)
+
+        # Declare a queue to receive prediction response
+        self.queue = self.channel.queue_declare(queue=configuration["in_queue"], exclusive=False)
+        self.queue_name = self.queue.method.queue
+        # Binding the exchange to the queue with specific routing
+        self.channel.queue_bind(exchange=self.exchange_name, queue=self.queue_name, routing_key=self.routing_key)
+
+    def on_request(self, ch, method, props, body):
+        # Process the data on request: sending back to host object
+        if self.host_object != None:
+            self.host_object.on_message_handling(ch, method, props, body)
+        else:
+            mess = json.loads(str(body.decode("utf-8")))
+            print(mess)
+
+    def start(self):
+        # Start rabbit MQ
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(queue=self.queue_name, on_message_callback=self.on_request, auto_ack=True)
+        self.channel.start_consuming()
+
+    def stop(self):
+        self.channel.stop_consuming()
+        self.channel.close()
+
+    def get_queue(self):
+        return self.queue.method.queue
---
+++ asynfed/commons/utils/queue_produce.py
@@ -0,0 +1,44 @@
+import pika, uuid
+
+class Amqp_Producer(object):
+    # Init an amqp client handling the connection to amqp servier
+    def __init__(self, configuration: dict, log: bool = False):
+        """
+        AMQP connector
+        configuration: a dictionary include broker and queue information
+        log: a bool flag for logging message if being set to True, default is False
+        """
+        self.conf = configuration
+        self.exchange_name = configuration["exchange_name"]
+        self.exchange_type = configuration["exchange_type"]
+        self.routing_key = configuration["routing_key"]
+        self.log_flag = log
+
+        # Connect to RabbitMQ host
+        if "amqps://" in configuration["end_point"]:
+            self.connection = pika.BlockingConnection(pika.URLParameters(configuration["end_point"]))
+        else:
+            self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=configuration["end_point"]))
+
+            # Create a channel
+        self.channel = self.connection.channel()
+
+        # Init an Exchange
+        self.channel.exchange_declare(exchange=self.exchange_name, exchange_type=self.exchange_type)
+
+    def send_data(self, body_mess, corr_id=None, routing_key=None, expiration=1000):
+        # Sending data to desired destination
+        # if sender is client, it will include the "reply_to" attribute to specify where to reply this message
+        # if sender is server, it will reply the message to "reply_to" via default exchange
+        if corr_id == None:
+            corr_id = str(uuid.uuid4())
+        if routing_key == None:
+            routing_key = self.routing_key
+        self.sub_properties = pika.BasicProperties(correlation_id=corr_id, expiration=str(expiration))
+        self.channel.basic_publish(exchange=self.exchange_name, routing_key=routing_key,
+                                   properties=self.sub_properties, body=body_mess)
+        # if self.log_flag:
+        #     self.mess_logging.log_request(body_mess,corr_id)
+
+    def get(self):
+        return self.conf
