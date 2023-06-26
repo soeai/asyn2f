291e30b1f48f0321bc6700884399fca7f603584c
dungcao <ctdung258@gmail.com>
2023-06-26 09:05:17 +0700

    Separate consume and produce queue (client_v2)

---
+++ asynfed/client_v2/algorithms/client_asyncfL.py
@@ -0,0 +1,297 @@
+import logging
+from datetime import datetime
+from time import sleep
+import numpy as np
+import pickle
+from asynfed.commons.conf import Config
+from asynfed.commons.messages import ClientNotifyModelToServer
+
+from ..client import Client
+from ..ModelWrapper import ModelWrapper
+from asynfed.commons.conf import Config
+
+import os
+LOGGER = logging.getLogger(__name__)
+
+# This is the proposed federated asynchronous training algorithm of our paper
+# More algorithms can be found at other files in this directory 
+class ClientAsyncFl(Client):
+    def __init__(self, model: ModelWrapper):
+        super().__init__()
+        '''
+        - model must be an instance of an inheritant of class ModelWrapper
+        - model.train_ds: require
+        - model.test_ds: optional
+        - train_ds and test_ds must in a format of a list of several batches
+            each batch contain x, y
+        - detail instruction (use as reference) of how to create a tensorflow model 
+            is found at asynfed/client/tensorflow/tensorflow_framework
+        - user is freely decided to follow the sample
+            or create their own model in their own platform (pytorch,...)
+        '''
+        self.model = model
+        self._local_data_size = self.model.data_size
+        self._local_qod = self.model.qod
+        self._train_acc = 0.0
+        self._train_loss = 0.0
+
+    def _get_model_dim_ready(self):
+       for images, labels in self.model.train_ds:
+            self.model.fit(images, labels)
+            break
+       
+    def _load_weights_from_file(self, file_name, folder):
+        full_path = folder + file_name
+        # with open(full_path, "rb") as f:
+        #     weights = pickle.load(f)
+        while not os.path.isfile(full_path):
+            print("*" * 20)
+            sleep(5)
+            print("Sleep 5 second when the model is not ready, then retry")
+            print("*" * 20)
+
+        with open(full_path, "rb") as f:
+            weights = pickle.load(f)
+        return weights
+
+    def train(self):
+        # for tensorflow model, there is some conflict in the dimension of 
+        # an initialized model and al already trained one
+        # temporarily fixed this problem
+        # by training the model before loading the global weights
+        self._get_model_dim_ready()
+
+        # check whether the client rejoin or training from the begining
+        if self._local_epoch <= 1:
+        # check whether there is more than one local model
+        # if just 1 or 0, go with the global model
+        # load the global model to set to be the client model weights 
+            LOGGER.info("Load global model to be current local model")
+            global_model_filename = self._global_model_name.split('/')[-1]
+            self.model.global_weights = self._load_weights_from_file(global_model_filename, Config.TMP_GLOBAL_MODEL_FOLDER)
+            self.model.set_weights(self.model.global_weights)
+        
+        # when client rejoin the training process
+        else:
+            print("*" * 20)
+            LOGGER.info("Rejoin the training process")
+            LOGGER.info("Loading the local weight...")
+            # load current local weight
+            current_local_model_filename = f'{self._client_id}_v{self._local_epoch}.pkl'
+            current_local_weights = self._load_weights_from_file(current_local_model_filename, Config.TMP_LOCAL_MODEL_FOLDER)
+            LOGGER.info("Loaded. ")
+            print("*" * 20)
+            self.model.set_weights(current_local_weights)
+
+            # if the receive global model version is equal to the save global model version
+            # just load the save current local version
+            if self._save_global_model_version == self._global_model_version:
+                LOGGER.info("The receive global model is equal to the save global model. Just load the current local weight and continue training")
+            else:
+                LOGGER.info("The receive global model is newer than the save one. Merging process needed.")
+                # merge these weight
+                previous_local_model_filename = f'{self._client_id}_v{self._local_epoch - 1}.pkl'
+                previous_local_weights = self._load_weights_from_file(previous_local_model_filename, Config.TMP_LOCAL_MODEL_FOLDER)
+                self.model.previous_weights = previous_local_weights
+                self.model.current_weights = current_local_weights
+                global_model_filename = self._global_model_name.split('/')[-1]
+                self.model.global_weights = self._load_weights_from_file(global_model_filename, Config.TMP_GLOBAL_MODEL_FOLDER)
+                self.__merge()
+
+
+        LOGGER.info("*" * 40)
+        LOGGER.info("ClientModel Start Training")
+        LOGGER.info("*" * 40)
+        # officially start the training process
+        # while True:
+        # quit after a number of epoch
+        # or after a sufficient period of time
+        from datetime import datetime
+        start_time = datetime.now()
+        for i in range(self.model.epoch):
+            # since the current mnist model is small, set some sleeping time
+            # to avoid overhead for the queue exchange and storage server
+            LOGGER.info(f"Sleep for {Config.SLEEPING_TIME} seconds to avoid overhead")
+            sleep(int(Config.SLEEPING_TIME))
+            # record some info of the training process
+            batch_num = 0
+            # if user require
+            # Tracking the training process every x samples 
+            # x define by user
+            batch_size = Config.BATCH_SIZE
+            tracking_period = Config.TRACKING_POINT
+            tracking_point = tracking_period
+
+            multiplier = 1
+            # reset loss and per after each epoch
+            self.model.reset_train_loss()
+            self.model.reset_train_performance()
+            self.model.reset_test_loss()
+            self.model.reset_test_performance()
+
+            # training per several epoch
+            LOGGER.info(f"Enter epoch {self._local_epoch}")
+            for images, labels in self.model.train_ds:
+                # Tracking the training process every x samples 
+                # x define by user
+                batch_num += 1
+                total_trained_sample = batch_num * batch_size
+                if total_trained_sample > tracking_point:
+                    LOGGER.info(f"Training up to {total_trained_sample} samples")
+                    multiplier += 1
+                    tracking_point = tracking_period * multiplier
+
+                # get the previous weights before the new training process within each batch
+                self.model.previous_weights = self.model.get_weights()
+                # training normally
+                self._train_acc, self._train_loss = self.model.fit(images, labels)
+
+                # merging when receiving a new global model
+                if self._new_model_flag:
+                    # before the merging process happens, need to retrieve current weights and global weights
+                    # previous, current and global weights are used in the merged process
+                    self.model.current_weights = self.model.get_weights()
+                    # load global weights from file
+                    global_model_path = Config.TMP_GLOBAL_MODEL_FOLDER + self._global_model_name
+                    while not os.path.isfile(global_model_path):
+                        print("*" * 20)
+                        sleep(5)
+                        print("Sleep 5 second when the model is not ready, then retry")
+                        print("*" * 20)
+
+                    with open(global_model_path, "rb") as f:
+                        self.model.global_weights = pickle.load(f)
+                    LOGGER.info(f"New model ? - {self._new_model_flag}")
+
+                    # # print out the performance of the global model
+                    # for test_images, test_labels in self.model.test_ds:
+                    #     self._test_acc, self._test_loss = self.model.evaluate(test_images, test_labels)
+
+                    LOGGER.info(
+                        f"Merging process happens at epoch {self._local_epoch}, batch {batch_num} when receiving the global version {self._current_local_version}, current global version {self._previous_local_version}")
+                    # merging
+                    self.__merge()
+                    # changing flag status
+                    self._new_model_flag = False
+                
+
+            if self.model.test_ds:
+                for test_images, test_labels in self.model.test_ds:
+                    self._test_acc, self._test_loss = self.model.evaluate(test_images, test_labels)
+            else:
+                self._test_acc, self._test_loss = 0, 0
+
+            
+
+            LOGGER.info(
+                f'Epoch: {self._local_epoch}'
+                f'\tLast Batch Train Accuracy: {self._train_acc * 100}, '
+                f'\tLast Batch Train Loss: {self._train_loss}, '
+                f'\tLast Batch Test Accuracy: {self._test_acc * 100}'
+                f'\tLast Batch Test Loss: {self._test_loss}, '
+            )
+
+
+            # Save weights locally after training
+            filename = f'{self._client_id}_v{self._local_epoch}.pkl'
+            save_location = Config.TMP_LOCAL_MODEL_FOLDER + filename
+            with open(save_location, 'wb') as f:
+                pickle.dump(self.model.get_weights(), f)
+            # Print the weight location
+            LOGGER.info(f'Saved weights to {save_location}')
+
+            # Only when the local model is save, local epoch is updated
+            self._local_epoch += 1
+
+            # Upload the weight to the storage (the remote server)
+            remote_file_path = 'clients/' + str(self._client_id) + '/' + filename
+            while True:
+                if self._storage_connector.upload(save_location, remote_file_path) is True:
+                    # After training, notify new model to the server.
+                    message = ClientNotifyModelToServer(
+                        client_id=self._client_id,
+                        timestamp=datetime.now().timestamp(),
+                        model_id=filename,
+                        weight_file=remote_file_path,
+                        # global_model_version_used=self._current_local_version,
+                        global_model_version_used=self._global_model_version,
+                        performance= self._train_acc,
+                        loss_value= self._train_loss,
+                    )
+
+                    print("*" * 20)
+                    print("Client Notify Model to Server")
+                    print(message)
+                    print("*" * 20)
+                    self.notify_model_to_server(message.serialize())
+                    LOGGER.info("ClientModel End Training, notify new model to server.")
+                    self.update_profile()
+                    break
+
+            # break before completing the intended number of epoch
+            # if the total training time excess some degree
+            # set by client
+            if self.model.delta_time:
+                delta_time = datetime.now() - start_time
+                delta_time_in_minute = delta_time.total_seconds() / 60
+                if delta_time_in_minute >= self.model.delta_time:
+                    break
+
+    def __merge(self):
+        LOGGER.info("MERGER weights.")
+        # updating the value of each parameter of the local model
+        # calculating the different to decide the formula of updating
+        # calculation is performed on each corresponding layout
+        # the different between the current weight and the previous weights
+
+        # why if scenario? --> this situation is for tensorflow model when the dim of 
+        # initialized model (fewer layers) and already trained model (more added layers)
+        # is conflicted 
+        if len(self.model.previous_weights) < len(self.model.current_weights):
+            # applying when entering the training step with an initialized model (8 layers), 
+            # not the pretrained one (18 layers)
+            # in the 1st batch of the 1st epoch
+            e_local = self.model.current_weights
+        else:
+            # perform calculation normally for all the other cases
+            e_local = [layer_a - layer_b for layer_a, layer_b in zip(self.model.current_weights, self.model.previous_weights)]
+
+        # e_local = [layer_a - layer_b for layer_a, layer_b in zip(self.model.current_weights, self.model.previous_weights)]
+
+        # the different between the global weights and the current weights
+        e_global = [layer_a - layer_b for layer_a, layer_b in zip(self.model.global_weights, self.model.current_weights)]
+        # get the direction list (matrix)
+        self.model.direction = [np.multiply(a, b) for a, b in zip(e_local, e_global)]
+
+        # Calculate alpha variable to ready for the merging process
+        # alpha depend on 
+        # qod, loss and data size from both the server and the local client
+        # qod
+        local_qod = self._local_qod
+        global_qod = self._global_avg_qod
+        # data size
+        local_size = self._local_data_size
+        global_size = self._global_model_update_data_size
+        # loss
+        local_loss = self._train_loss
+        global_loss = self._global_avg_loss
+        # calculate alpha
+        alpha = ( (local_qod*local_size) / (local_qod*local_size + global_qod*global_size) + local_loss/(local_loss + global_loss) )
+        alpha = alpha / 2
+
+        # create a blank array to store the result
+        self.model.merged_weights = [np.zeros(layer.shape) for layer in self.model.current_weights]
+        # base on the direction, global weights and current local weights
+        # updating the value of each parameter to get the new local weights (from merging process)
+        # set the index to move to the next layer
+        i = 0
+        for (local_layer, global_layer, direction_layer) in zip(self.model.current_weights, self.model.global_weights, self.model.direction):
+            # access each element in each layer
+            # np.where(condition, true, false)
+            merged_layer = np.where(direction_layer >=0, global_layer, (1 - alpha) * global_layer + alpha * local_layer)
+            self.model.merged_weights[i] = merged_layer
+            i += 1
+
+        # set the merged_weights to be the current weights of the model
+        self.model.set_weights(self.model.merged_weights)
+
---
+++ asynfed/client_v2/client.py
@@ -0,0 +1,153 @@
+import json
+import logging
+import os
+import threading
+import uuid
+from time import sleep
+from abc import abstractmethod
+from asynfed.client.client_storage_connector import ClientStorage
+from asynfed.commons.conf import RoutingRules
+from asynfed.commons.messages import ClientInit
+from asynfed.commons.messages.client_init_connect_to_server import SysInfo
+from asynfed.commons.utils.queue_consume import Amqp_Consumer
+from asynfed.commons.utils.queue_produce import Amqp_Producer
+
+LOGGER = logging.getLogger(__name__)
+
+lock = threading.Lock()
+
+
+class Client(object):
+    def __init__(self, config):
+        # Dependencies
+        self._global_chosen_list = None
+        self._save_global_avg_qod = None
+        self._save_global_avg_loss = None
+        self._save_global_model_update_data_size = None
+        self._save_global_model_version = None
+        self._local_data_size = 0
+        self._local_qod = 0.0
+        self._train_loss = 0.0
+
+        self._previous_local_version = 0
+        self._current_local_version = 0
+
+        self._global_model_version = None
+
+        self._global_model_name = None
+        self._storage_connector = None
+
+        self._local_epoch = 0
+        # merging process
+        self._global_avg_loss = None
+        self._global_avg_qod = None
+        self._global_model_update_data_size = None
+
+        # variables.
+        self._client_id = ""
+        self._is_training = False
+        self._session_id = ""
+        self._client_identifier = str(uuid.uuid4())
+        self._new_model_flag = False
+        self._is_registered = False
+
+        # Config.QUEUE_NAME = self._client_identifier
+
+        # if there is no profile.json file, then create a new one.
+        if not os.path.exists("profile.json"):
+            self.create_profile()
+        else:
+            self.load_profile()
+
+        self.log: bool = True
+        # init_config("client")
+
+        self.queue_concumer = Amqp_Consumer(config['queue_consumer'], self)
+        self.queue_producer = Amqp_Producer(config['queue_producer'])
+
+    def on_message_handling(ch, method, props, body):
+        pass
+
+    @abstractmethod
+    def train(self):
+        pass
+
+    def create_message(self):
+        data = {
+            "session_id": self._session_id,
+            "client_id": self._client_id,
+            "global_model_name": self._global_model_name,
+            "local_epoch": self._local_epoch - 1,
+            "local_qod": self._local_qod,
+            "save_global_model_version": self._global_model_version,
+            "save_global_model_update_data_size": self._global_model_update_data_size,
+            "save_global_avg_loss": self._global_avg_loss,
+            "save_global_avg_qod": self._global_avg_qod,
+
+            # "local_data_size": self._local_data_size,
+            # "local_qod": self._local_qod,
+            # "train_loss": self._train_loss,
+        }
+        return data
+
+    def create_profile(self):
+        data = self.create_message()
+        with open("profile.json", "w") as outfile:
+            json.dump(data, outfile)
+
+    def update_profile(self):
+        data = self.create_message()
+        with open("profile.json", "w") as outfile:
+            json.dump(data, outfile)
+
+    # load client information from profile.json function
+    def load_profile(self):
+        try:
+            with open("profile.json") as json_file:
+                data = json.load(json_file)
+                self._session_id = data["session_id"]
+                self._client_id = data["client_id"]
+                self._global_model_name = data["global_model_name"]
+                self._local_epoch = data["local_epoch"]
+                self._local_qod = data["local_qod"]
+
+                self._save_global_model_version = data["save_global_model_version"]
+                self._save_global_model_update_data_size = data["save_global_model_update_data_size"]
+                self._save_global_avg_loss = data["save_global_avg_loss"]
+                self._save_global_avg_qod = data["save_global_avg_qod"]
+
+                # self._local_data_size = data["local_data_size"]
+                # self._train_loss = data["train_loss"]
+        except Exception as e:
+            print(e)
+
+    def notify_model_to_server(self, message):
+        self.queue_producer.send_data(message)
+
+    def init_connect_to_server(self, message):
+        self.queue_producer.send_data(message)
+
+    def publish_init_message(self, data_size=10000, qod=0.2):
+        message = ClientInit(
+            client_identifier=self._client_identifier,
+            session_id=self._session_id,
+            client_id=self._client_id,
+            sys_info=SysInfo(),
+            data_size=data_size,
+            qod=qod
+        )
+        print("-" * 20)
+        print("Init message of client")
+        print(message)
+        print("-" * 20)
+        self.init_connect_to_server(message.serialize())
+
+    def start_training_thread(self):
+        if not self._is_training:
+            LOGGER.info("Start training thread.")
+            training_thread = threading.Thread(
+                target=self.train,
+                name="client_training_thread")
+
+            self._is_training = True
+            training_thread.start()
