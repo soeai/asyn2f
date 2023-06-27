36f58eb413585351072c748efbeba0ac42bf3a20
vtn13042000 <vtn13042000>
2023-06-27 21:47:48 +0700

    add bash file to run experiment for cifar10 on ttu server

---
+++ training_process/cifar_dataset/client/234_gpu2_rejoin.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd asynfed_5_chunks_2
+
+# # Remove directories if they exist
+# if [ -d "weights" ]; then
+#     rm -r "weights"
+# fi
+
+# if [ -d "logs" ]; then
+#     rm -r "logs"
+# fi
+
+# # Remove file if it exists
+# if [ -f "profile.json" ]; then
+#     rm "profile.json"
+# fi
+
+# Activate conda environment
+conda activate asynfed
+
+# Run the python file
+python run_client.py --gpu_index 1 --chunk_index 5
