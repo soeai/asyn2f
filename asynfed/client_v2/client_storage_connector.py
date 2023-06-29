f45db92680fce50a914ed9849ba1ace8c3dfea2d
dungcao <ctdung258@gmail.com>
2023-06-29 08:53:08 +0700

    Implementing v2

--- asynfed/client_v2/client_storage_connector.py
+++ asynfed/client_v2/client_storage_connector.py
@@ -2,7 +2,7 @@ from asynfed.commons.utils import AWSConnector
 
 
 class ClientStorage(AWSConnector):
-    def __init__(self, parent):
+    def __init__(self, parent=None):
         super().__init__(parent)
 
 
