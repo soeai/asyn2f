e752c372560327f3e1c358ae72e8b2e085bba3a6
Thai Le Quang <tlim@Thais-MacBook-Pro.local>
2023-07-04 17:01:32 +0700

    update server and client accept arguments

--- flower/client.py
+++ flower/client.py
@@ -1,6 +1,7 @@
 
 import os
 import sys
+import argparse
 root = os.path.dirname(os.getcwd())
 sys.path.append(root)
 
@@ -10,51 +11,70 @@ import tensorflow as tf
 from training_process.cifar_dataset.client.resnet18_5_chunks.data_preprocessing import preprocess_dataset
 from training_process.cifar_dataset.client.resnet18 import Resnet18
 
-
-args = sys.argv
-chunk = int(args[1])
-
-# HYPERPARAMETERS
-learning_rate = 1e-1
-epoch = 200
-batch_size = 128
-
-
-test_path = 'data/test_set.pickle'
-train_path = f'data/chunk_{chunk}.pickle'
-train_ds, data_size = preprocess_dataset(train_path, batch_size=128, training=True)
-
-print('data size:', data_size)
-x_train, y_train = load_to_numpy_array(train_path)
-x_test, y_test = load_to_numpy_array(test_path)
-
-y_train = tf.keras.utils.to_categorical(y_train, 10)
-y_test = tf.keras.utils.to_categorical(y_test, 10)
+def start_client(args):
+    chunk = args.chunk
+    if args.gpu is not None:
+        try:
+            tf.config.set_visible_devices(tf.config.list_physical_devices('GPU')[args.gpu], 'GPU')
+            print("Using GPU: ", tf.config.list_physical_devices('GPU')[args.gpu])
+        except:
+            print("GPU not found, use CPU instead")
+    else:
+        print("Using CPU")
+    # HYPERPARAMETERS
+    learning_rate = 1e-1
+    epoch = 200
+    batch_size = 128
 
 
-print('x_train shape:', x_train.shape, 'y_train shape:', y_train.shape, 'x_test shape:', x_test.shape, 'y_test shape:', y_test.shape)
-model = Resnet18(input_features= (32, 32, 3), 
-                 output_features= 10,
-                 lr=learning_rate,
-                 decay_steps=epoch*data_size/batch_size)
-model.build(input_shape=(None, 32, 32, 3))
-# model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
-model.compile("adam", "categorical_crossentropy", metrics=["accuracy"])
+    test_path = 'data/test_set.pickle'
+    train_path = f'data/chunk_{chunk}.pickle'
+    train_ds, data_size = preprocess_dataset(train_path, batch_size=128, training=True)
 
-class CifarClient(fl.client.NumPyClient):
-    def get_parameters(self, config):
-        return model.get_weights()
+    print('data size:', data_size)
+    x_train, y_train = load_to_numpy_array(train_path)
+    x_test, y_test = load_to_numpy_array(test_path)
 
-    def fit(self, parameters, config):
-        model.set_weights(parameters)
-        model.fit(x_train, y_train, epochs=1, batch_size=32, steps_per_epoch=10)
-        return model.get_weights(), len(x_train), {}
-
-    def evaluate(self, parameters, config):
-        model.set_weights(parameters)
-        loss, accuracy, _, _, _, _ = model.evaluate(x_test, y_test, batch_size=batch_size, return_dict=False)
-        return loss, len(x_test), {"accuracy": accuracy}
+    y_train = tf.keras.utils.to_categorical(y_train, 10)
+    y_test = tf.keras.utils.to_categorical(y_test, 10)
 
 
+    print('x_train shape:', x_train.shape, 'y_train shape:', y_train.shape, 'x_test shape:', x_test.shape, 'y_test shape:', y_test.shape)
+    model = Resnet18(input_features= (32, 32, 3), 
+                    output_features= 10,
+                    lr=learning_rate,
+                    decay_steps=epoch*data_size/batch_size)
+    model.build(input_shape=(None, 32, 32, 3))
+    # model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+    model.compile("adam", "categorical_crossentropy", metrics=["accuracy"])
 
-fl.client.start_numpy_client(server_address="localhost:8080", client=CifarClient())
+    class CifarClient(fl.client.NumPyClient):
+        def get_parameters(self, config):
+            return model.get_weights()
+
+        def fit(self, parameters, config):
+            model.set_weights(parameters)
+            model.fit(x_train, y_train, epochs=1, batch_size=32, steps_per_epoch=10)
+            return model.get_weights(), len(x_train), {}
+
+        def evaluate(self, parameters, config):
+            model.set_weights(parameters)
+            loss, accuracy, _, _, _, _ = model.evaluate(x_test, y_test, batch_size=batch_size, return_dict=False)
+            return loss, len(x_test), {"accuracy": accuracy}
+
+
+
+    fl.client.start_numpy_client(server_address=args.address, client=CifarClient())
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Federated Learning Client")
+        
+    parser.add_argument("--gpu", type=int, default=None, help="Specify the GPU index")
+    parser.add_argument("--chunk", type=int, default=1, help="Specify the chunk size")
+    parser.add_argument("--address", type=str, default="localhost:8080", help="Specify the server address")
+
+    args = parser.parse_args()
+    start_client(args)
+
+
--- flower/server.py
+++ flower/server.py
@@ -1,8 +1,24 @@
 import flwr as fl
+import argparse
 
 
-# Start Flower server
-fl.server.start_server(
-    server_address="0.0.0.0:8080",
-    config=fl.server.ServerConfig(num_rounds=3),
-)
+def start_server(args):
+    strategy = fl.server.strategy.FedAvg(
+        min_fit_clients=args.min_worker,
+    )
+
+    fl.server.start_server(
+        server_address=args.address,
+        config=fl.server.ServerConfig(args.num_rounds),
+        strategy=strategy,
+    )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Federated Learning Server")
+
+    parser.add_argument("--address", type=str, default="localhost:8080", help="Specify the port number")
+    parser.add_argument("--min_worker", type=int, default=2, help="Specify the minimum number of workers")
+    parser.add_argument("--num_rounds", type=int, default=10, help="Specify the minimum number of workers")
+
+    args = parser.parse_args()
+    start_server(args)
