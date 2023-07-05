e1efd82c3f3d4c5dafaa492475e3c70ded007826
vtn13042000 <thaonguyen.vuong.16@gmail.com>
2023-07-05 21:35:40 +0700

    add reconnect mechanism for queue

--- asynfed/commons/utils/queue_consumer.py
+++ asynfed/commons/utils/queue_consumer.py
@@ -1,48 +1,47 @@
-import pika, json
+import pika, json, logging
 
+LOGGER = logging.getLogger(__name__)
+logging.getLogger('pika').setLevel(logging.WARNING)
+LOGGER.setLevel(logging.INFO)
 
 class AmqpConsumer(object):
-    # Init an amqp client handling the connection to amqp servier
     def __init__(self, configuration: dict, host_object: object = None):
-        self.conf = configuration
         self.host_object = host_object
+        self.conf = configuration
         self.exchange_name = configuration["exchange_name"]
         self.exchange_type = configuration["exchange_type"]
         self.routing_key = configuration["routing_key"]
-        self._connect()
-    
-    def _connect(self):
-        # Connect to RabbitMQ host
+        self._setup_connection()
+
+    def _setup_connection(self):
         if "amqps://" in self.conf["end_point"]:
             self.connection = pika.BlockingConnection(pika.URLParameters(self.conf["end_point"]))
         else:
             self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.conf["end_point"]))
-
-        # Create a channel
         self.channel = self.connection.channel()
-
-        # Init an Exchange
         self.channel.exchange_declare(exchange=self.exchange_name, exchange_type=self.exchange_type)
-
-        # Declare a queue to receive prediction response
         self.queue = self.channel.queue_declare(queue=self.conf["queue_name"], exclusive=False)
         self.queue_name = self.queue.method.queue
-        # Binding the exchange to the queue with specific routing
         self.channel.queue_bind(exchange=self.exchange_name, queue=self.queue_name, routing_key=self.routing_key)
 
     def on_request(self, ch, method, props, body):
-        # Process the data on request: sending back to host object
         if self.host_object != None:
             self.host_object.on_message_received(ch, method, props, body)
         else:
             mess = json.loads(str(body.decode("utf-8")))
-            print(mess)
+            LOGGER.info(mess)
 
     def start(self):
-        # Start rabbit MQ
-        self.channel.basic_qos(prefetch_count=1)
-        self.channel.basic_consume(queue=self.queue_name, on_message_callback=self.on_request, auto_ack=True)
-        self.channel.start_consuming()
+        try:
+            self.channel.basic_qos(prefetch_count=1)
+            self.channel.basic_consume(queue=self.queue_name, on_message_callback=self.on_request, auto_ack=True)
+            self.channel.start_consuming()
+        # except pika.exceptions.StreamLostError as e:
+        except Exception as e:
+            LOGGER.info(e)
+            LOGGER.exception("Connection lost. Reconnecting...")
+            self._reconnect()
+            self.start()
 
     def stop(self):
         self.channel.stop_consuming()
@@ -50,3 +49,6 @@ class AmqpConsumer(object):
 
     def get_queue(self):
         return self.queue.method.queue
+
+    def _reconnect(self):
+        self._setup_connection()
