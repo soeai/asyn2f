7e9942c768adffb561bb16ba9394c7318044c245
dungcao <ctdung258@gmail.com>
2023-07-10 14:21:46 +0700

    Edit loss for flower...

--- flower/client_234_gpu2.py
+++ flower/client_234_gpu2.py
@@ -62,21 +62,33 @@ def start_client(args):
 
     # Create the SGD optimizer with L2 regularization
     optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate_fn, momentum=0.9)
-    regularizer = tf.keras.regularizers.l2(lambda_value)
+
+    def custom_loss_with_l2_reg():
+        def loss(y_true, y_pred):
+            l2_loss = tf.add_n([tf.nn.l2_loss(w) for w in model.trainable_weights])
+            return tf.keras.losses.categorical_crossentropy(y_true, y_pred) + lambda_value * l2_loss
+
+        return loss
 
     # Compile the model
-    model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'], 
-                loss_weights=None, weighted_metrics=None, run_eagerly=None, 
+    model.compile(optimizer=optimizer, loss='custom_loss_with_l2_reg', metrics=['accuracy'],
+                loss_weights=None, weighted_metrics=None, run_eagerly=None,
                 steps_per_execution=None)
+    # regularizer = tf.keras.regularizers.l2(lambda_value)
+    #
+    # # Compile the model
+    # model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'],
+    #             loss_weights=None, weighted_metrics=None, run_eagerly=None,
+    #             steps_per_execution=None)
     # Build the model
     model.build(input_shape=(None, 32, 32, 3))
 
     # Apply L2 regularization to applicable layers
-    for layer in model.layers:
-        if isinstance(layer, tf.keras.layers.Conv2D) or isinstance(layer, tf.keras.layers.Dense):
-            layer.kernel_regularizer = regularizer
-        if hasattr(layer, 'bias_regularizer') and layer.use_bias:
-            layer.bias_regularizer = regularizer
+    # for layer in model.layers:
+    #     if isinstance(layer, tf.keras.layers.Conv2D) or isinstance(layer, tf.keras.layers.Dense):
+    #         layer.kernel_regularizer = regularizer
+    #     if hasattr(layer, 'bias_regularizer') and layer.use_bias:
+    #         layer.bias_regularizer = regularizer
 
 
     class CifarClient(fl.client.NumPyClient):
