11dddf794b0ae27fbea7655fdcad54c92cfe8b87
vtn13042000 <thaonguyen.vuong.16@gmail.com>
2023-07-17 17:18:33 +0700

    clear unneccessary files

--- test/partial_tests/Server.py
+++
@@ -1,9 +0,0 @@
-from asynfed.commons.conf import Config
-from asynfed.server.server import Server
-from asynfed.server.server_queue_connector import ServerQueueConnector
-
-Config.QUEUE_NAME = "server_queue"
-
-test_Server = ServerQueueConnector()
-
-test_Server.run_queue()
--- test/partial_tests/client_consumer.py
+++
@@ -1,6 +0,0 @@
-from asynfed.client.client import Client
-from asynfed.commons.conf import Config
-
-Config.QUEUE_NAME = "client_queue"
-client_consumer = Client()
-client_consumer.run_queue()
