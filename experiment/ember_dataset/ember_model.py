d5a975e7e084f963c84adabef4e51fa4fca88dd4
vtn13042000 <thaonguyen.vuong.16@gmail.com>
2023-08-13 12:10:21 +0700

    modify ember model - consider class weighted for imbalanced dataset (non iid)

--- experiment/ember_dataset/ember_model.py
+++ experiment/ember_dataset/ember_model.py
@@ -2,24 +2,22 @@ import tensorflow as tf
 from asynfed.client.frameworks.tensorflow import TensorflowSequentialModel
 from asynfed.client.config_structure import LearningRateConfig
 
-
 class CustomCosineDecay(tf.keras.optimizers.schedules.LearningRateSchedule):
     def __init__(self, initial_learning_rate, decay_steps, min_learning_rate=0.001):
-        # super(CustomCosineDecay, self).__init__()
         self.initial_learning_rate = initial_learning_rate
         self.decay_steps = decay_steps
         self.min_learning_rate = min_learning_rate
 
     def __call__(self, step):
-        
+
         def true_fn():
             return self.min_learning_rate
-        
+
         def false_fn():
             cosine_decay = 0.5 * (1 + tf.math.cos(tf.constant(np.pi) * tf.cast(step, tf.float32) / tf.cast(self.decay_steps, tf.float32)))
             decayed = (1 - self.min_learning_rate) * cosine_decay + self.min_learning_rate
             return self.initial_learning_rate * decayed
-        
+
         condition = tf.greater_equal(step, self.decay_steps)
         return tf.cond(condition, true_fn, false_fn)
 
@@ -29,6 +27,7 @@ class CustomCosineDecay(tf.keras.optimizers.schedules.LearningRateSchedule):
             "decay_steps": self.decay_steps,
             "min_learning_rate": self.min_learning_rate,
         }
+
 
 class F1Score(tf.keras.metrics.Metric):
     def __init__(self, name="f1_score", **kwargs):
@@ -50,13 +49,30 @@ class F1Score(tf.keras.metrics.Metric):
         self.recall.reset_states()
 
 
+class WeightedBinaryCrossentropy(tf.keras.losses.Loss):
+    def __init__(self, class_weight, **kwargs):
+        super(WeightedBinaryCrossentropy, self).__init__(**kwargs)
+        self.class_weight = tf.convert_to_tensor(class_weight, dtype=tf.float32)
+
+    def call(self, y_true, y_pred):
+        y_true = tf.reshape(y_true, [-1, 1])  # Reshape y_true to align with y_pred
+        weights = tf.gather(self.class_weight, tf.cast(y_true, tf.int32))
+        entropy = tf.keras.losses.binary_crossentropy(y_true, y_pred)
+        return tf.reduce_mean(entropy * weights)
+
+
 
 class EmberModel(TensorflowSequentialModel):
-    def __init__(self, input_features = 2381, output_features = 1, lr_config: dict = None):
+    def __init__(self, input_features = 2381, output_features = 1, lr_config: dict = None, class_weight: dict= None):
         lr_config = lr_config or {}
         self.lr_config = LearningRateConfig(**lr_config)
         print("lr config in the resnet model")
         print(self.lr_config.to_dict())
+        if class_weight:
+          self.class_weight = class_weight
+        else:
+          self.class_weight = None
+
         super().__init__(input_features=input_features, output_features=output_features)
 
         print(f"Learning rate right now (step = 0) is: {self.get_learning_rate()}")
@@ -91,6 +107,8 @@ class EmberModel(TensorflowSequentialModel):
         return self.model(x)
 
     def create_loss_object(self):
+        if self.class_weight:
+          return WeightedBinaryCrossentropy(class_weight= self.class_weight)
         return tf.keras.losses.BinaryCrossentropy()
 
     def create_optimizer(self):
@@ -99,7 +117,7 @@ class EmberModel(TensorflowSequentialModel):
             print(f"Create optimizer with fix learning rate: {optimizer.lr.numpy()}")
 
         else:
-            lr_scheduler = CustomCosineDecay(initial_learning_rate= self.lr_config.initial_lr, 
+            lr_scheduler = CustomCosineDecay(initial_learning_rate= self.lr_config.initial_lr,
                                             decay_steps= self.lr_config.decay_steps,
                                             min_learning_rate= self.lr_config.min_lr)
 
@@ -112,22 +130,20 @@ class EmberModel(TensorflowSequentialModel):
 
 
     def create_train_metric(self):
-        return F1Score(name='train_f1'), tf.keras.metrics.Mean(name='train_loss')
+        return F1Score(name='train_acc_f1_score'), tf.keras.metrics.Mean(name='train_loss')
 
     def create_test_metric(self):
-        return F1Score(name='test_f1'), tf.keras.metrics.Mean(name='test_loss')
-
+        return tf.keras.metrics.BinaryAccuracy(name='test_accuracy'), tf.keras.metrics.Mean(name='test_loss')
 
     def get_train_performance(self):
-        return float(self.train_f1.result())
+        return float(self.train_performance.result())
 
     def get_test_performance(self):
-        return float(self.test_f1.result())
-
+        return float(self.test_performance.result())
 
     def get_train_loss(self):
         return float(self.train_loss.result())
 
-
     def get_test_loss(self):
         return float(self.test_loss.result())
+
